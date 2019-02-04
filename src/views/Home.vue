1b281d7cb1f1ad1445298b48bd97fcba655e8fea
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2019-02-04 10:31:01 +0100

    Chore(Clean): Remove Unwanted Files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>d</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
+<script>
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
