4454e21d64ba78c12413de78d73411adb29a816b
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2019-02-04 10:46:25 +0100

    Chore(VueUIKit): Integrate VueUIKit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from "vue";
+import Vuikit from "vuikit";
+import VuikitIcons from "@vuikit/icons";
+import "@vuikit/theme";
+
 import App from "@/App.vue";
 import router from "@/router/";
 import store from "@/store/";
 import "@/serviceworkers/registerServiceWorker";
 
+Vue.use(Vuikit);
+Vue.use(VuikitIcons);
 Vue.config.productionTip = false;
 
 new Vue({
